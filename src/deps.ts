76edfc3fe9f10654898e81f6c744b090c9ec6785
Scott Côté <scott.cote@hey.com>
2023-05-14 11:51:06 -0400

    Clean up deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,15 +1,15 @@
 // std
-export * as stdPath from "https://deno.land/std@0.178.0/path/mod.ts";
-export * as stdDatetime from "https://deno.land/std@0.178.0/datetime/mod.ts";
-export * as stdLog from "https://deno.land/std@0.178.0/log/mod.ts";
-export * as stdFs from "https://deno.land/std@0.178.0/fs/mod.ts";
-export { LogLevelNames } from "https://deno.land/std@0.178.0/log/levels.ts";
-export * as stdYaml from "https://deno.land/std@0.178.0/encoding/yaml.ts";
-export * as stdColors from "https://deno.land/std@0.178.0/fmt/colors.ts";
-export * as stdAsync from "https://deno.land/std@0.178.0/async/mod.ts";
-export * as stdDeepMerge from "https://deno.land/std@0.178.0/collections/deep_merge.ts";
 export * as stdAsserts from "https://deno.land/std@0.178.0/testing/asserts.ts";
+export * as stdAsync from "https://deno.land/std@0.178.0/async/mod.ts";
 export * as stdBytes from "https://deno.land/std@0.178.0/bytes/mod.ts";
+export * as stdColors from "https://deno.land/std@0.178.0/fmt/colors.ts";
+export * as stdDatetime from "https://deno.land/std@0.178.0/datetime/mod.ts";
+export * as stdDeepMerge from "https://deno.land/std@0.178.0/collections/deep_merge.ts";
+export * as stdFs from "https://deno.land/std@0.178.0/fs/mod.ts";
+export * as stdLog from "https://deno.land/std@0.178.0/log/mod.ts";
+export { LogLevelNames } from "https://deno.land/std@0.178.0/log/levels.ts";
+export * as stdPath from "https://deno.land/std@0.178.0/path/mod.ts";
+export * as stdYaml from "https://deno.land/std@0.178.0/encoding/yaml.ts";
 
 // postgres
 export { default as postgres } from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
@@ -18,10 +18,10 @@ export { default as postgres } from "https://deno.land/x/postgresjs@v3.3.3/mod.j
 export { connect as redis } from "https://deno.land/x/redis@v0.29.2/mod.ts";
 export type { Redis as RedisConnection, RedisConnectOptions } from "https://deno.land/x/redis@v0.29.2/mod.ts";
 
+// misc
+export { outdent } from "https://deno.land/x/outdent@v0.8.0/mod.ts";
+
 // npm
 export { default as shell } from "npm:shell-quote@1.8.0";
 export type { PartialDeep, SetRequired } from "npm:type-fest@3.6.0";
 export { serializeError } from "npm:serialize-error@11.0.0";
-
-// dax
-export { outdent } from "https://deno.land/x/outdent@v0.8.0/mod.ts";
