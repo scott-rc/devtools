de9e82d5e5dbc269f8db4cf2587d2b2ce534fea8
Scott Côté <scott.cote@hey.com>
2023-05-14 11:59:25 -0400

    Update deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,27 +1,27 @@
 // std
-export * as stdAsserts from "https://deno.land/std@0.178.0/testing/asserts.ts";
-export * as stdAsync from "https://deno.land/std@0.178.0/async/mod.ts";
-export * as stdBytes from "https://deno.land/std@0.178.0/bytes/mod.ts";
-export * as stdColors from "https://deno.land/std@0.178.0/fmt/colors.ts";
-export * as stdDatetime from "https://deno.land/std@0.178.0/datetime/mod.ts";
-export * as stdDeepMerge from "https://deno.land/std@0.178.0/collections/deep_merge.ts";
-export * as stdFs from "https://deno.land/std@0.178.0/fs/mod.ts";
-export * as stdLog from "https://deno.land/std@0.178.0/log/mod.ts";
-export { LogLevelNames } from "https://deno.land/std@0.178.0/log/levels.ts";
-export * as stdPath from "https://deno.land/std@0.178.0/path/mod.ts";
-export * as stdYaml from "https://deno.land/std@0.178.0/encoding/yaml.ts";
+export * as stdAsserts from "https://deno.land/std@0.187.0/testing/asserts.ts";
+export * as stdAsync from "https://deno.land/std@0.187.0/async/mod.ts";
+export * as stdBytes from "https://deno.land/std@0.187.0/bytes/mod.ts";
+export * as stdColors from "https://deno.land/std@0.187.0/fmt/colors.ts";
+export * as stdDatetime from "https://deno.land/std@0.187.0/datetime/mod.ts";
+export * as stdDeepMerge from "https://deno.land/std@0.187.0/collections/deep_merge.ts";
+export * as stdFs from "https://deno.land/std@0.187.0/fs/mod.ts";
+export * as stdLog from "https://deno.land/std@0.187.0/log/mod.ts";
+export { LogLevelNames } from "https://deno.land/std@0.187.0/log/levels.ts";
+export * as stdPath from "https://deno.land/std@0.187.0/path/mod.ts";
+export * as stdYaml from "https://deno.land/std@0.187.0/yaml/mod.ts";
 
 // postgres
-export { default as postgres } from "https://deno.land/x/postgresjs@v3.3.3/mod.js";
+export { default as postgres } from "https://deno.land/x/postgresjs@v3.3.4/mod.js";
 
 // redis
-export { connect as redis } from "https://deno.land/x/redis@v0.29.2/mod.ts";
-export type { Redis as RedisConnection, RedisConnectOptions } from "https://deno.land/x/redis@v0.29.2/mod.ts";
+export { connect as redis } from "https://deno.land/x/redis@v0.29.4/mod.ts";
+export type { Redis as RedisConnection, RedisConnectOptions } from "https://deno.land/x/redis@v0.29.4/mod.ts";
 
 // misc
 export { outdent } from "https://deno.land/x/outdent@v0.8.0/mod.ts";
 
 // npm
-export { default as shell } from "npm:shell-quote@1.8.0";
-export type { PartialDeep, SetRequired } from "npm:type-fest@3.6.0";
+export { default as shell } from "npm:shell-quote@1.8.1";
+export type { PartialDeep, SetRequired } from "npm:type-fest@3.10.0";
 export { serializeError } from "npm:serialize-error@11.0.0";
